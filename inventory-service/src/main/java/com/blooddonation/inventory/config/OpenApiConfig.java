a9dd9221f825f0811279d234c056787a362ed795
Lakshitha <lakshitha320@gmail.com>
2026-08-18 22:53:22 +0530

    Add Swagger OpenAPI configuration to all microservices for interactive API docs on port 8080-8084

---
+++ inventory-service/src/main/java/com/blooddonation/inventory/config/OpenApiConfig.java
@@ -0,0 +1,31 @@
+﻿package com.blooddonation.inventory.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String apiKeySchemeName = "apiKeyAuth";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Blood Inventory Service API")
+                        .version("1.0.0")
+                        .description("Blood Inventory Service API for Blood Donation System"))
+                .addSecurityItem(new SecurityRequirement().addList(apiKeySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(apiKeySchemeName,
+                                new SecurityScheme()
+                                        .name("X-API-KEY")
+                                        .type(SecurityScheme.Type.APIKEY)
+                                        .in(SecurityScheme.In.HEADER)));
+    }
+}
